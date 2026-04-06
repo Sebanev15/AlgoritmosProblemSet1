ccd60d7de5c6aab70df79ab1cbb0dc70df083cd7
joaquindedios <joaquin.dedios@correo.ucu.edu.uy>
2026-04-06 16:24:23 -0300

    operaciones de conjunto interseccion y diferencia

---
+++ src/main/java/org/example/Ejercicio17/Conjunto.java
@@ -0,0 +1,44 @@
+package org.example.Ejercicio17;
+
+public class Conjunto<T extends Comparable<T>> extends ListaEnlazada<T> implements TDAConjunto<T>{
+    private ListaEnlazada<T> conjunto;
+    public Conjunto() {
+        conjunto = new ListaEnlazada<>();
+    }
+    
+    @Override
+    public TDAConjunto<T> union(TDAConjunto<T> otrConjunto){
+        TDAConjunto<T> resultado= new Conjunto<>();
+        Nodo<T> nodoActual=this.head;
+
+        while (nodoActual!= null){
+            resultado.agregar(nodoActual.dato);
+            nodoActual=nodoActual.siguiente;
+        }
+
+
+        return null;
+    }
+    public TDAConjunto<T> interseccion(TDAConjunto<T> otrConjunto){
+        TDAConjunto<T> resultado =new Conjunto<>();
+        Nodo<T> nodoActual=this.head;
+        while(nodoActual!=null){
+            if (otrConjunto.buscar(nodoActual.dato()) != null){
+                resultado.agregar(nodoActual.dato);
+            }
+        }nodoActual=nodoActual.siguiente;
+        return resultado;
+    }
+    public TDAConjunto<T> diferencia (TDAConjunto<T> otrConjunto){
+        TDAConjunto<T> resultado= new Conjunto<>();
+        Nodo<T> nodoActual=this.head;
+        while (nodoActual!=null){
+            if (otrConjunto.buscar(nodoActual.dato())== null){
+                resultado.agregar(nodoActual.dato);
+            }
+    }       nodoActual=nodoActual.siguiente;
+        return resultado;
+    }
+    
+    
+}
--- src/main/java/org/example/Ejercicio17/Nodo.java
+++ src/main/java/org/example/Ejercicio17/Nodo.java
@@ -1,5 +1,7 @@
  package org.example.Ejercicio17;
 
+import java.util.function.Predicate;
+
 public class Nodo<T> {
     public T dato;
     public Nodo<T> siguiente;
@@ -8,4 +10,8 @@ public class Nodo<T> {
         this.siguiente = siguiente;
     }
     //metodo equals que invoque el equals del dato tipo t.
+    public Predicate<T> dato() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dato'");
+    }
 }
